025167ecadc7a91342d51f0a6c521277d922aefe
Juniorkz13 <junior.13.kz@hotmail.com>
2021-09-15 14:49:54 -0300

    JS

---
+++ node/moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'Fala pessoal' // isso poderá ser exportado
+exports.bemVindo = 'Bem vindo ao node!' // isso poderá ser exportado
+module.exports.ateLogo = 'Até o próximo exemplo!' // isso poderá ser exportado
---
+++ node/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloA') // Importa o moduloA
+const moduloB = require('./moduloB') // Importa o moduloB
+
+console.log(moduloA.ola) // acessa o moduloA
+console.log(moduloA.bemVindo) // acessa o moduloA
+console.log(moduloA.ateLogo) // acessa o moduloA
+console.log(moduloA)
+
+console.log(moduloB.bomDia) // acessa o moduloB
+console.log(moduloB.boaNoite()) // acessa o moduloB
+console.log(moduloB)
